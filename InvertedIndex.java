e8744c1d143102bb97e2bf1d24da0ccd1057134c
Oscar Ashburn <89231886+O-LavenderAshburn@users.noreply.github.com>
2024-05-17 19:59:24 +1200

    Update InvertedIndex.java

--- InvertedIndex.java
+++ InvertedIndex.java
@@ -1,3 +1,8 @@
+/**
+ *Inverted Index class to set up jobs
+ * @author Oscar Ashburn
+ * @version 1.0.0
+ */
 package invertedindex;
 
 import org.apache.commons.io.FileUtils;
@@ -5,6 +10,7 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapreduce.Counter;
 import org.apache.hadoop.mapreduce.Job;
 
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
@@ -36,35 +42,62 @@ public class InvertedIndex extends Configured implements Tool{
 		 	 }
 
 			Configuration conf = new Configuration();
-
+			  //set delimiter
 			  conf.set("textinputformat.record.delimiter","</Document>");
-			  
+			  //Setup Job
 			 Job job = Job.getInstance(conf, "inverted index");
-			 //add your implementation here
-
 			 job.setJarByClass(InvertedIndex.class);
-
 			 job.setOutputKeyClass(Text.class);
 			 job.setOutputValueClass(Text.class);
 
+			 //Set classes  for __
+			 //Mapper
 			 job.setMapperClass(InvertedIndexMapper.class);
+			 //Reducer
 			 job.setReducerClass(InvertedIndexReducer.class);
-
-			 job.setNumReduceTasks(2);
-
+			 //Comparator
+			 job.setSortComparatorClass(InvertedIndexComparator.class);
+			 //Combiner	//Yet to be implemented\\
+			 // job.setCombinerClass(InvertedIndexCombiner.class);
+			 //Partitioner
+			 job.setPartitionerClass(InvertedIndexPartitioner.class);
+			 //Set reduce tasks
+			 job.setNumReduceTasks(4);
+			 //set input and output
 			 job.setInputFormatClass(TextInputFormat.class);
 			 job.setOutputFormatClass(TextOutputFormat.class);
-
+			 //Set input and output paths
 			 FileInputFormat.setInputPaths(job, new Path(args[0]));
 			 FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
-//			 return job.waitForCompletion(true)?0:1;
+
 			 boolean success = job.waitForCompletion(true);
+			 //Get custom counters if successful
+			 if(success){
+				 //Count of number of stop words
+				 Counter counter = job.getCounters().findCounter(Counters.STOP_WORDS);
+				 System.out.println("Stop words:	" + counter.getValue());
+				 //Count of number of total characters
+				 Counter counter2 = job.getCounters().findCounter(Counters.TOTAL_CHARACTERS);
+				 System.out.println("Total characters:	" + (Long)counter2.getValue());
+				 //Total word count
+				 Counter counter3= job.getCounters().findCounter(Counters.WORD_COUNT);
+				 System.out.println("Word Count:	" + (Long)counter3.getValue());
+				 //Get counter of all words that start with A or a
+				 Counter counter4 = job.getCounters().findCounter(Counters.A_WORDS);
+				 System.out.println("A Words:	" + (Long)counter4.getValue());
+				 //Get counter of all words that start with Z or z
+				 Counter counter5 = job.getCounters().findCounter(Counters.Z_WORDS);
+				 System.out.println("Z Words:	" + (Long)counter4.getValue());
+
+			 }
+			 //return success
 			 return success ? 0 : 1;
 
 		  }
 		  
 		  public static void main(String[] args) throws Exception {
+
 			  String outputFile  = "/home/oa57/Desktop/COMPX553/assignment-six-hadoop-inverted-2024/output";
 			  if (Files.isDirectory(Paths.get(outputFile))) {
 				  FileUtils.forceDelete(new File(outputFile));
